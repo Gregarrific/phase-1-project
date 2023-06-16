789f811f3ebf7281b6c6facb2f9114f6b5d95f59
Gregory Dutton <greg.dutton@gmail.com>
2023-06-16 09:48:56 -0700

    Change initiateCat function to initiateDifficulty and complete function w/ event listener

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //Global variables
-let playerName;
+let playerName, difficulty, introMsg;
 let timerEnd = false;
-let introMsg;
 const introReset = 'Ready to play?  Enter your name below to start a new game.  Good luck!'
 const apiUrl = `http://localhost:3000/results`; //temporary while testing
 //Event listeners
@@ -21,17 +20,26 @@ function initiatePlayer(e) {
     }
     playerName = formInput;
     introMsg = introReset;
-    initiateCat();
+    initiateDifficulty();
 }
-function initiateCat() {
+function initiateDifficulty() {
     mainContainer.innerHTML = "";
     let heading = document.createElement('h3');
     heading.innerText = `2. ${playerName}, choose your difficulty level:`;
     let difficultySelect = document.createElement('select');
+    difficultySelect.id = 'selection';
     difficultySelect.innerHTML = 
-        `<option value="easy">Easy</option>
+        `<option value="" disabled selected>Choose wisely</option>
+        <option value="easy">Easy</option>
         <option value="medium">Medium</option>
         <option value="hard">Hard</option>`;
     mainContainer.appendChild(heading);
     mainContainer.appendChild(difficultySelect);
+    difficultySelect.addEventListener('change', e => {
+        difficulty = e.target.value;
+        initiateCategory();
+    });
+}
+function initiateCategory() {
+    mainContainer.innerHTML = "";
 }
