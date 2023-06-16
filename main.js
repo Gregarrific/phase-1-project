2c8e3a41f860f28d5faae7d6ed1430c0d1190e55
Gregory Dutton <greg.dutton@gmail.com>
2023-06-16 08:45:43 -0700

    Add submit event listener and initiatePlayer function

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ let playerName;
 let timerEnd = false;
 const apiUrl = `http://localhost:3000/results`; //temporary while testing
 //Event listeners
-
+document.addEventListener('submit', e => initiatePlayer(e));
 //Functions
+function initiatePlayer(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formInput = form.querySelector('#player-name').value;   
+    console.log(formInput);
+}
